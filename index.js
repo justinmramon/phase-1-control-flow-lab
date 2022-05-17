cf27ce75ea789af4596aa27f7b946013ef9978c9
Justin M. Ramon <justinmramon@gmail.com>
2022-05-17 08:18:01 -0500

    commit tree 18514fa352e8c60ddfcb2c39552f16cb356f73de

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue <= 400)
+  return 'This one is on me!'
+  else if (someValue > 400 && someValue < 2000)
+  return 'That will be twenty bucks.'
+  else if (someValue > 2000 && someValue <= 2499)
+  return 'I will gladly take your thirty bucks.'
+  else if (someValue > 2500)
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityName){
   // Write your code here!
+  return (cityName === 'NYC' ? 'Ok, sounds good.' : 'No go.')
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+    switch(generosity){
+      case 'generous' : return 'Thank you so much.'
+      case 'not as generous' : return 'Thank you.'
+      default : return 'Bye.'
+   }
 }
